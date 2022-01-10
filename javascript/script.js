adbd1756a77f23f267f303c0c5a2981d0fa92fd6
hfranco2 <francohenrique10@gmail.com>
2022-01-10 11:00:21 -0300

    t

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+var i = 0;
+i=11;
+console.log(i);
+console.log({
+    name:"mayk",
+    idade: 36,
+    andar: function(){
+        console.log("andar")
+    }
+    
+})
+console.log(["myke", 36])
+//testando o escopo do var
+console.log(x)
+{var x = 0;}
+console.log(x)
+//escopo do let
+console.log(y)
+{const y = 0;
+    console.log(y)}
+console.log(y)
+//escopo da const
+console.log(z)
+{let z = 0;
+    console.log(z)}
+console.log(z)
